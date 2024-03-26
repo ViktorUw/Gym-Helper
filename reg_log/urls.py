523e0def2ff1ffdc07f20441593978d9cf33cd2b
Viktor <338791@uwr.edu.pl>
2024-03-26 01:21:47 +0100

    Adding messages.html

--- reg_log/urls.py
+++ reg_log/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.homepage, name='homepage'),
     path("registration/", views.user_registration, name='registration'),
-    path("login/", views.user_login, name='login')
+    path("login/", views.user_login, name='login'),
+    path("logout/", views.user_logout, name='logout'),
 ]
--- reg_log/views.py
+++ reg_log/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.urls import reverse
@@ -18,8 +18,15 @@ def user_registration(request):
         form = UserRegForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'You are registered successfully')
             return HttpResponseRedirect(reverse('login'))
+        else:
+            for field in form.errors:
+                field_errors = ', '.join([str(err) for err in form.errors[field]])
+                messages.error(request, f"{field}: {field_errors}")
             
+
+                
     
 
     return render(request, 'reg_log/registration.html', {'form':form})
@@ -33,8 +40,10 @@ def user_login(request):
                                 password = cd['password1'])
             if user is not None:
                 login(request, user)
+                messages.success(request, 'You are logged in successfully')
                 return HttpResponseRedirect(reverse('mainPage'))
-            
+            else:
+                messages.error(request, 'Invalid login or password')
 
     else:
         form = UserLogForm()
@@ -42,3 +51,9 @@ def user_login(request):
 
 
     return render(request, 'reg_log/login.html', {'form':form})
+
+
+def user_logout(request):
+    logout(request)
+    messages.success(request,'You are logged out successfully')
+    return HttpResponseRedirect(reverse('login'))
